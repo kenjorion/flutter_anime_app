48b52f7f3a24ea75f78651e4da11643ab75842a1
Thierry Kg <t.kaing13@gmail.com>
2020-07-10 18:16:41 +0200

    - add listview

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -36,10 +36,35 @@ class Home extends StatelessWidget {
           ),
         ),
         Expanded(
-            child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.black
-                )
+            child: ListView(
+              padding: const EdgeInsets.all(8),
+              children: <Widget>[
+                Container(
+                  height: 100,
+                  color: Colors.amber[600],
+                  child: const Center(child: Text('Entry A')),
+                ),
+                Container(
+                  height: 100,
+                  color: Colors.amber[500],
+                  child: const Center(child: Text('Entry B')),
+                ),
+                Container(
+                  height: 100,
+                  color: Colors.amber[100],
+                  child: const Center(child: Text('Entry C')),
+                ),
+                Container(
+                  height: 100,
+                  color: Colors.amber[500],
+                  child: const Center(child: Text('Entry B')),
+                ),
+                Container(
+                  height: 100,
+                  color: Colors.amber[100],
+                  child: const Center(child: Text('Entry C')),
+                ),
+              ]
             )
         )
       ],
