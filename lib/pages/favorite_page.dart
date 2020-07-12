c442bd17537d423aba13b2967324ee5ad9fea90d
Thierry Kg <t.kaing13@gmail.com>
2020-07-12 19:21:00 +0200

    - implement search bar

--- lib/pages/favorite_page.dart
+++ lib/pages/favorite_page.dart
@@ -23,9 +23,13 @@ class FavoritePage extends StatefulWidget {
 
 class _FavoritePageState extends State<FavoritePage> {
 
+  String searchTerm;
+  bool isSearchTerm;
   Future<Anime> futureAnime;
   Future<List> futureMoviesList;
   Future<List> futureSeriesList;
+  Future<List> futureSearchMoviesList;
+  Future<List> futureSearchSeriesList;
 
   Future<Anime> fetchAnime() async {
     final response = await http.get('https://api.jikan.moe/v3/anime/1');
@@ -72,74 +76,91 @@ class _FavoritePageState extends State<FavoritePage> {
     }
   }
 
+  Future<List> fetchSearchMoviesList() async {
+    final response = await http.get('https://api.jikan.moe/v3/top/anime/1/movie');
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      Map<String, dynamic> responseResult = json.decode(response.body);
+      return (responseResult['top'] as List)
+          .map((data) => new Anime.fromJson(data))
+          .toList();
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load anime list (movies)');
+    }
+  }
+
   @override
   void initState() {
     super.initState();
+    searchTerm = "";
+    isSearchTerm = false;
     futureAnime = fetchAnime();
     futureMoviesList = fetchMoviesList();
     futureSeriesList = fetchSeriesList();
   }
 
+  void _handleOnSearchButtonClick() {
+    setState(() {
+      isSearchTerm = true;
+    });
+  }
+
+  void _handleOnSearchInputChanged(String term) {
+    setState(() {
+      searchTerm = term;
+    });
+  }
+
+  Widget _buildFutureBuilderList(Future<List<dynamic>> futureList, int nextScreenIndex) {
+    return FutureBuilder<List>(
+      future: futureList,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, index) {
+              Anime anime = snapshot.data[index];
+              return GestureDetector(
+                child: Text(anime.title),
+                onTap: () => {
+                  widget.onScreenChanged(nextScreenIndex)
+                },
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        // By default, show a loading spinner.
+        return Center(
+            child: CircularProgressIndicator()
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
 
-    Widget futureBuilderMoviesList = FutureBuilder<List>(
-      future: futureMoviesList,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return ListView.builder(
-            itemCount: snapshot.data.length,
-            itemBuilder: (context, index) {
-              Anime anime = snapshot.data[index];
-              return GestureDetector(
-                child: Text(anime.title),
-                onTap: () => {
-                  widget.onScreenChanged(MyApp.detailsScreenIndex)
-                },
-              );
-            },
-          );
-        } else if (snapshot.hasError) {
-          return Text("${snapshot.error}");
-        }
-        // By default, show a loading spinner.
-        return Center(
-            child: CircularProgressIndicator()
-        );
-      },
-    );
-    Widget futureBuilderSeriesList = FutureBuilder<List>(
-      future: futureSeriesList,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return ListView.builder(
-            itemCount: snapshot.data.length,
-            itemBuilder: (context, index) {
-              Anime anime = snapshot.data[index];
-              return GestureDetector(
-                child: Text(anime.title),
-                onTap: () => {
-                  widget.onScreenChanged(MyApp.detailsScreenIndex)
-                },
-              );
-            },
-          );
-        } else if (snapshot.hasError) {
-          return Text("${snapshot.error}");
-        }
-        // By default, show a loading spinner.
-        return Center(
-            child: CircularProgressIndicator()
-        );
-      },
-    );
+    Widget futureBuilderMoviesList = _buildFutureBuilderList(futureMoviesList, MyApp.detailsScreenIndex);
+    Widget futureBuilderSeriesList = _buildFutureBuilderList(futureSeriesList, MyApp.detailsScreenIndex);
     Widget futureBuilderResultList;
 
-    if (widget.currentTopTabIndex == MyApp.topTabMoviesIndex)
-      futureBuilderResultList = futureBuilderMoviesList;
+    if (isSearchTerm && searchTerm.trim().isNotEmpty) {
 
-    if (widget.currentTopTabIndex == MyApp.topTabSeriesIndex)
-      futureBuilderResultList = futureBuilderSeriesList;
+      
+
+    } else {
+
+      if (widget.currentTopTabIndex == MyApp.topTabMoviesIndex)
+        futureBuilderResultList = futureBuilderMoviesList;
+
+      if (widget.currentTopTabIndex == MyApp.topTabSeriesIndex)
+        futureBuilderResultList = futureBuilderSeriesList;
+    }
 
     return Column(
       children: <Widget>[
@@ -157,18 +178,52 @@ class _FavoritePageState extends State<FavoritePage> {
               ]
           ),
           child: TabBar(
-              tabs: widget.topTabs,
-              controller: widget.topController,
-              labelColor: Theme.of(context).primaryColor,
-              indicatorColor: Theme.of(context).primaryColor,
-              unselectedLabelColor: Colors.grey,
-              onTap: (index) {
-                widget.onTopTabChanged(index);
-              },
+            tabs: widget.topTabs,
+            controller: widget.topController,
+            labelColor: Theme.of(context).primaryColor,
+            indicatorColor: Theme.of(context).primaryColor,
+            unselectedLabelColor: Colors.grey,
+            onTap: (index) {
+              widget.onTopTabChanged(index);
+            },
+          ),
+        ),
+        Container(
+          height: 50,
+          margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0, bottom: 40.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                  flex: 10,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Cherche ton anime",
+                        contentPadding: EdgeInsets.symmetric(horizontal: 10.0)
+                    ),
+                    onChanged: _handleOnSearchInputChanged,
+                  )
+              ),
+              Expanded(
+                  flex: 2,
+                  child: Center(
+                      child: Ink(
+                        decoration: const ShapeDecoration(
+                          color: Colors.lightBlue,
+                          shape: CircleBorder(),
+                        ),
+                        child: IconButton(
+                          icon: Icon(Icons.search),
+                          color: Colors.white,
+                          onPressed: _handleOnSearchButtonClick,
+                        ),
+                      )
+                  )
+              ),
+            ],
           ),
         ),
         Expanded(
-            flex: 3,
             child: futureBuilderResultList
         )
       ],
